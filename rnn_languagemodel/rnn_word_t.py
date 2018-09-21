be214ca2bedd8e3b9dc53c46abb64b2630d96213
AnsonSz <1047053860@qq.com>
2018-09-21 18:17:35 +0800

    rnn训练语言模型初始化

---
+++ rnn_languagemodel/rnn_word_t.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import rnn
+import random
+import time
+from collections import Counter
+
+start_time = time.time()
+
+def elapsed(sec):
+    pass
